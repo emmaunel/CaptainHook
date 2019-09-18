ad13bb3078991757f22d5ffa94d5bf1b400da509
Emmanuel <aadewale123@gmail.com>
2019-09-17 21:57:01 -0400

    updates

--- client.sh
+++ client.sh
@@ -7,6 +7,6 @@ while true; do
     echo ${encode}
     touch $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1)
     git add -A > /dev/null
-    git commit -m ${encode} /dev/null
+    git commit -m ${encode} > /dev/null
     git push | grep "remote: " | awk '{print $2}'
 done
