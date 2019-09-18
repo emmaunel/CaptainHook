6e5924efc36b7e2a21dbd9acf5ac7798ac7a0c33
Emmanuel <aadewale123@gmail.com>
2019-09-17 21:48:42 -0400

    Update client.sh

--- client.sh
+++ client.sh
@@ -7,6 +7,8 @@ while true; do
     echo ${encode}
     touch $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1)
     git add -A > /dev/null
-    git commit -m ${encode} /dev/null
+    git commit -m ${encode} > /dev/null
+    #can't figure out how to hide the pushing info
+    #I tried moving to /dev/null but no luck
     git push | grep "remote: " | awk '{print $2}'
 done
